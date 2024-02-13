0c6636c81b13a4afbeb72183ff46f5e3d0896b41
Camacho <rafaelcamachofitti15@gmail.com>
2024-02-13 02:16:36 -0300

    Feat/CRUD completo

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from "express"
 
 const app = express()
 
+// Middleware para parsear o corpo das requisições JSON
+app.use(express.json());
+
 const livros =[
     {
         id: 1,
@@ -13,12 +16,41 @@ const livros =[
     },    
 ]
 
+function findBookById(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    });
+}
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
 
+app.get("/livros/:id", (req, res) => {
+    const index = findBookById(req.params.id);
+    res.status(200).json(livros[index]);
+});
+
 app.get("/livros", (req, res) => {
     res.status(200).json(livros);
-})
+});
+
+app.post("/livros", (req, res) => {
+    const livro = req.body;
+    livros.push(livro);
+    res.status(201).json(livro);
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = findBookById(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros[index]);
+});
+
+app.delete("/livros/:id", (req, res) => {
+    const index = findBookById(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).json({message: "Livro removido com sucesso"});
+});
 
 export default app
